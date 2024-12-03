6dddd9cbebdf060509e2762478491db8edc7ebb6
Jay Pratt <jp10010101010000@gmail.com>
2024-12-03 15:04:12 +0200

    tmp

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    use pg_parse::ast::Node;
+
+    let args: Vec<String> = std::env::args().collect();
+    let path = &args[1];
+    let input = std::fs::read_to_string(&path).expect("?");
+    let result = pg_parse::parse(&input);
+    assert!(result.is_ok());
+    let result = result.unwrap();
+    assert!(matches!(*&result[0], Node::SelectStmt(_)));
+}
+
+#[test]
+fn sanity_test() {
+    use pg_parse::ast::Node;
+
+    let result = pg_parse::parse("SELECT * FROM contacts");
+    assert!(result.is_ok());
+    let result = result.unwrap();
+    assert!(matches!(*&result[0], Node::SelectStmt(_)));
+
+    // We can also convert back to a string, if the `str` feature is enabled (enabled by default).
+    assert_eq!(result[0].to_string(), "SELECT * FROM contacts");
+}
